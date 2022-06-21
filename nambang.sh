a3135bbf0b3c7623579808ef5bc2a0ba92286012
kosasih85 <91346465+kosasih85@users.noreply.github.com>
2022-06-21 15:44:40 +0700

    Update nambang.sh

--- nambang.sh
+++ nambang.sh
@@ -2,7 +2,7 @@
 echo "Delete File lama dulu ya";
 rm "/root/ccminer/kosasih.sh"
 echo "download file";
-wget -P /root/ccminer/ https://gist.githubusercontent.com/kosasih85/a901dbaf3ba4dde7b9abfce590e14302/raw/dce3f53493d33d9a84a283f6819674fd12e765a9/kosasih.sh
+wget -P /root/ccminer/ https://raw.githubusercontent.com/kosasih85/verus/main/kosasih.sh
 echo "Kasih Permisi file sukses";
 chmod +rwxrwxrwx "/root/ccminer/kosasih.sh"
 echo "Run Filenya";
