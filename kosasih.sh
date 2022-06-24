7034bc77c41f44f055e8ee0fb737a13fadb4ffa1
kosasih85 <91346465+kosasih85@users.noreply.github.com>
2022-06-24 11:08:09 +0700

    Update kosasih.sh

--- kosasih.sh
+++ kosasih.sh
@@ -1,1 +1,1 @@
-/root/ccminer/ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u DLxmNZjPdjpPF28bsdH44DnMPotFaQkVNN -p c=DOGE,ID=stb -t 4
+/root/ccminer/ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u DLxmNZjPdjpPF28bsdH44DnMPotFaQkVNN -p c=DOGE,m=party.,ID=stb -t 4
