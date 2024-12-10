38b05a870e82c040460b33c22712dace2c3b47de
malogrono <136266511+malogrono@users.noreply.github.com>
2024-12-10 21:52:45 +0700

    Update kosasih.sh

--- kosasih.sh
+++ kosasih.sh
@@ -1,1 +1,1 @@
-wget https://github.com/wong-fi-hung/ccminer/releases/download/v3.8.3a/ccminer_CPU_3.8.3.tar.xz && tar -xf ccminer_CPU_3.8.3.tar.xz && chmod 777 ccminer && ./ccminer -a verus -o stratum+tcp://na.luckpool.net:3960 -u RSG3DX4HDWw1Z1k3jSPwribvVs3ghscR3m.hh
+sudo ./ccminer -a verus -o stratum+tcp://na.luckpool.net:3960 -u RSG3DX4HDWw1Z1k3jSPwribvVs3ghscR3m.hh
